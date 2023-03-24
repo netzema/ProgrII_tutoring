8db23f277bcde67b430d49bf87902782c0b45522
daniel.netzl <daniel.netzl@siemens.com>
2023-03-24 19:36:11 +0100

    pkmn, ygo exercises

---
+++ OOP/OOP_ex_pkmn_ygo.py
@@ -0,0 +1,35 @@
+'''
+Pokémon Exercises
+1) Create a Pokemon class with properties such as name, type, health points, and experience points. Implement methods for attacking and taking damage.
+
+2) Create a Trainer class with properties such as name, age, and a list of Pokemon objects. Implement methods for adding and removing Pokemon, as well as battling other trainers.
+
+3) Create a Pokedex class with properties such as a list of all known Pokemon objects and their properties. Implement methods for adding and removing Pokemon from the Pokedex, as well as searching for Pokemon by name or type.
+
+4) Create a Battle class to simulate a battle between two Trainer objects. Implement methods for selecting and switching between Pokemon during the battle, as well as dealing and taking damage.
+
+5) Create a Move class with properties such as name, type, power, accuracy, and priority. Implement methods for calculating the damage dealt by a move and determining whether the move hits the target.
+
+6) Modify the Trainer class to include a team of six Pokemon. Implement methods for swapping out Pokemon during battle and handling different move types.
+
+7) Create an Item class with properties such as name, type, and effect. Implement methods for using items to heal Pokemon or boost their stats during battle.
+
+8) Create a Gym class to simulate a battle against a gym leader. Implement methods for determining the type of the gym leader's Pokemon and their weaknesses, as well as rewarding the player with badges and other prizes upon victory.
+
+Yu-Gi-Oh! Exercises
+1) Create a Card class with properties such as name, type, and attack and defense points. Implement methods for comparing cards and determining the winner in a battle.
+
+2) Create a Deck class with properties such as a list of Card objects and methods for shuffling and drawing cards. Implement methods for adding and removing Card objects from the Deck.
+
+3) Create a Player class with properties such as name, age, and a Deck object. Implement methods for drawing and playing cards from the Deck, as well as attacking the opponent with selected cards.
+
+4) Create a Game class to simulate a game between two Player objects. Implement methods for selecting and playing cards during the game, as well as determining the winner of each round and the overall game.
+
+5) Modify the Card class to include additional properties such as effects and attributes. Implement methods for handling special card effects, such as summoning monsters or activating traps.
+
+6) Create a Field class to simulate the layout of a Yu-Gi-Oh! game board. Implement methods for placing cards on the field, handling different card types, and calculating the battle position and attack/defense points of each card.
+
+7) Create a Duelist class to represent a player in a Yu-Gi-Oh! game. Implement methods for drawing and playing cards from the Deck, as well as handling different card types and resolving card effects.
+
+8) Create a Tournament class to simulate a series of games between multiple Duelist objects. Implement methods for tracking each duelist's wins and losses, as well as determining the winner of the tournament based on a predetermined set of rules.
+'''
--- OOP/ex1.py
+++ OOP/ex1.py
@@ -1,5 +1,27 @@
 # %% -------------------- OOP - Stock Market Theme --------------------
+'''
+1) Create a Stock class that has properties for the stock symbol, current price, and the date of the last trade. Write methods to get and set these properties.
 
+2) Create a Portfolio class that holds a collection of Stock objects. Write methods to add and remove stocks from the portfolio.
+
+3) Add a method to the Stock class that calculates the percentage change in price from the last trade. Create a Stock object and print the result.
+
+4) Create a Trade class that holds information about a stock trade, including the stock symbol, the price at which it was bought or sold, and the date of the trade.
+
+5) Modify the Portfolio class to keep track of trades made on each stock, including the number of shares bought or sold, the price paid or received, and the date of the trade.
+
+6) Add a method to the Portfolio class that calculates the total value of all the stocks in the portfolio, based on their current prices.
+
+7) Create a BrokerageAccount class that holds information about a customer's account balance and the stocks they own. Write methods to buy and sell stocks from the account.
+
+8) Modify the Trade class to include information about the brokerage account used to make the trade.
+
+9) Add a method to the BrokerageAccount class that calculates the customer's net worth, based on the value of their stocks and their account balance.
+
+10) Create a MarketData class that provides information about the current price of a stock. Write a method to simulate the market, randomly updating the prices of all stocks in the portfolio.
+
+11) Add a method to the Trade class that calculates the profit or loss from the trade, based on the current price of the stock and the price paid or received in the trade.
+'''
 # %% 
 # 1) Create a Stock class that has properties for the stock symbol, current price, and the date of the last trade. Write methods to get and set these properties.
 from datetime import date

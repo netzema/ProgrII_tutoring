fe675fd06796b44a12f61818f5072771b96ebbe9
daniel.netzl <daniel.netzl@siemens.com>
2023-05-25 14:27:34 +0200

    added new exercises

--- solutions/SQL Alchemy/sql_alchemy_solutions.py
+++ solutions/SQL Alchemy/sql_alchemy_solutions.py
@@ -95,7 +95,7 @@ users_with_keyword_posts = session.query(User).join(Post).filter(Post.title.like
 users = session.query(User).all()
 for user in users:
     print(user.name, user.email)
-# %%
+# %% Exercise 6
 class Address(Base):
     __tablename__ = 'addresses'
     id = Column(Integer, primary_key=True)
@@ -117,7 +117,7 @@ session.commit()
 # Query a user and retrieve all their addresses
 user = session.query(User).filter_by(name='John').first()
 user_addresses = user.addresses
-# %% Additional Exercises
+# %% Exercise 7
 from sqlalchemy import func
 
 # Calculate the total number of users in the database
@@ -127,7 +127,7 @@ total_users = session.query(func.count(User.id)).scalar()
 max_address_count = session.query(func.count(Address.id)).join(User).group_by(User.id).order_by(func.count(Address.id).desc()).first()
 users_with_max_addresses = session.query(User).join(Address).group_by(User.id).having(func.count(Address.id) == max_address_count[0]).all()
 
-# %%
+# %% Exercise 8
 # Retrieve the top 5 users with the highest user IDs
 top_users = session.query(User).order_by(User.id.desc()).limit(5).all()
 
@@ -136,7 +136,7 @@ alphabetical_users = session.query(User).order_by(User.name).all()
 
 # Retrieve the youngest user based on their birthdate
 youngest_user = session.query(User).order_by(User.dob.asc()).first()
-# %%
+# %% Exercise 9
 # Retrieve users whose names contain the letter 'a'
 users_with_a = session.query(User).filter(User.name.like('%a%')).all()
 
@@ -147,7 +147,7 @@ users_with_multiple_addresses = session.query(User).join(Address).group_by(User.
 start_date = date(1990, 1, 1)
 end_date = date(2000, 12, 31)
 users_in_date_range = session.query(User).filter(User.dob.between(start_date, end_date)).all()
-# %%
+# %% Exercise 10
 # Update the street name of a specific address
 address = session.query(Address).filter_by(id=1).first()
 address.street = 'New Street'
@@ -157,12 +157,11 @@ session.commit()
 address = session.query(Address).filter_by(id=2).first()
 session.delete(address)
 session.commit()
-# %%
+# %% Exercise 11
 # Retrieve users and their corresponding addresses using an explicit join
 users_with_addresses = session.query(User, Address).join(Address).all()
 
 # Retrieve users who have at least one address in a specific city
 city = 'City A'
 users_in_city = session.query(User).join(Address).filter(Address.city == city).all()
-
 # %%
